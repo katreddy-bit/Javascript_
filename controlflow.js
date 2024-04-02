6c6022ba806d56f338c922a9c1abc4ec12ad57ea
Katreddy-bit <katreddyshivakumarreddy@gmail.com>
2024-04-02 10:25:59 +0530

    first commit

---
+++ controlflow.js
@@ -0,0 +1,36 @@
+// let hour=17;
+// if (hour>=6 && hour<12) console.log('good morning');
+// else if (hour>=12 && hour<16) console.log('Good afternoon ');
+// else console.log('good evening');
+
+// let user='moderator';
+// switch (user){
+//     case 'guest':
+//         console.log('guest');
+//         break;
+//     case 'moderator':
+//         console.log('moderator user');
+//         break;
+//     default:
+//         console.log('unknown user');
+// }
+// if (user==='guest') console.log('guest user');
+// else if (user==='moderator') console.log('moderator user');
+// else console.log('unknown user');
+// for(let i=0;i<=5;i++){
+//     if(i%2 ==0)console.log(i);
+// }
+
+// let i=0;
+// while(i<5){
+//     if(i%2 ==0)console.log(i);
+//     i++;
+// }
+
+
+// do- while 
+let i=9;
+do{
+    if(i%2!==0) console.log(i);
+    i++;
+}while(i<5);
---
+++ index.js
@@ -0,0 +1,16 @@
+// // read write execute
+// //00000100
+// //00000010
+// //00000001
+// const read = 4;
+// const write =2;
+// const execute=1;
+// let mypermission=0;
+// mypermission=mypermission|read|write|execute;
+// console.log(mypermission);
+// let msg=(mypermission&read) ? 'Yes':'No';
+// console.log(msg);
+// let x = 10;
+
+// let y = (x > 5) && (x < 15); 
+// console.log(y)
